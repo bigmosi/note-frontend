b20a9d0d5765b7f5b56aac4bb695c45a866f9ddc
bigmosi <kinyeramo@gmail.com>
2023-07-20 01:14:22 +0300

    fixed issues with drag and drop

--- src/components/NoteOrganization.jsx
+++ src/components/NoteOrganization.jsx
@@ -1,93 +1,86 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import { useParams } from 'react-router-dom';
+import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+const Note = ({ note, index, moveNote }) => {
+  const ref = React.useRef(null);
+  const [, drop] = useDrop({
+    accept: 'NOTE',
+    hover(item, monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      moveNote(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  });
+
+  const [, drag] = useDrag({
+    type: 'NOTE',
+    item: { id: note._id, index },
+  });
+
+  drag(drop(ref));
+
+  return (
+    <div ref={ref}>
+      <h3>{note.title}</h3>
+      <p>{note.content}</p>
+      <ul>
+        {note.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const NoteOrganization = () => {
   const [notes, setNotes] = useState([]);
   const { categoryId } = useParams();
 
-  // Fetch notes from the backend on component mount
   useEffect(() => {
     fetchNotes();
   }, []);
 
   const fetchNotes = async () => {
     try {
-      const response = await axios.get(`http://localhost:4000/api/v1/categories/${categoryId}/notes`);
+      const response = await axios.get(
+        `http://localhost:4000/api/v1/categories/${categoryId}/notes`
+      );
       setNotes(response.data.notes);
     } catch (error) {
       console.error('Error fetching notes:', error);
     }
   };
 
-  const onDragEnd = (result) => {
-    const { destination, source } = result;
-
-    // Check if the drag-and-drop was successful
-    if (!destination) return;
-
-    // Reorder the notes based on the drag-and-drop result
-    const updatedNotes = Array.from(notes);
-    const [draggedNote] = updatedNotes.splice(source.index, 1);
-    updatedNotes.splice(destination.index, 0, draggedNote);
-
-    // Update the frontend state to reflect the new order immediately
+  const moveNote = (dragIndex, hoverIndex) => {
+    const updatedNotes = [...notes];
+    const draggedNote = updatedNotes[dragIndex];
+    updatedNotes.splice(dragIndex, 1);
+    updatedNotes.splice(hoverIndex, 0, draggedNote);
     setNotes(updatedNotes);
-
-    // Make an API call to update the order in the backend
-    axios
-      .put(`http://localhost:4000/api/v1/categories/${categoryId}/notes/reorder`, {
-        noteIds: updatedNotes.map((note) => note._id),
-      })
-      .then((response) => {
-        // Handle successful response if needed
-        console.log('Note order updated successfully!', response.data);
-        // Show a success message to the user, update UI, etc.
-      })
-      .catch((error) => {
-        console.error('Error updating note order:', error);
-        // Rollback the frontend state if the API call fails
-        setNotes([...notes]);
-        // Show an error message to the user, handle UI accordingly, etc.
-      });
   };
 
   return (
     <div>
       <h2>My Notes</h2>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="notes">
-          {(provided) => (
-            <div ref={provided.innerRef} {...provided.droppableProps}>
-              {notes && notes.length > 0 ? (
-                notes.map((note, index) => (
-                  <Draggable key={note._id} draggableId={note._id} index={index}>
-                    {(provided) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        <h3>{note.title}</h3>
-                        <p>{note.content}</p>
-                        <ul>
-                          {note.tags.map((tag) => (
-                            <li key={tag}>{tag}</li>
-                          ))}
-                        </ul>
-                      </div>
-                    )}
-                  </Draggable>
-                ))
-              ) : (
-                <p>No notes found.</p>
-              )}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+      <DndProvider backend={HTML5Backend}>
+        {notes.length > 0 ? (
+          notes.map((note, index) => (
+            <Note key={note._id} note={note} index={index} moveNote={moveNote} />
+          ))
+        ) : (
+          <p>No notes found.</p>
+        )}
+      </DndProvider>
     </div>
   );
 };
