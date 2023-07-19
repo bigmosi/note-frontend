676bd3ef2bddf6884750494cfe28eb3d2d1fe045
bigmosi <kinyeramo@gmail.com>
2023-07-19 20:38:09 +0300

    creating notes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import './App.css';
-import NoteCreationInterface from './comonents/NoteCreationInterface';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import NoteOrganization from './components/NoteOrganization';
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  // Fetch notes from the backend on component mount
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const fetchNotes = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000/api/v1/notes');
+      setNotes(response.data.notes);
+    } catch (error) {
+      console.error('Error fetching notes:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <NoteCreationInterface />
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          {/* Add navigation links */}
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/categories/:categoryId" element={<NoteOrganization notes={notes} />} />
+        {/* Add other routes as needed */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
