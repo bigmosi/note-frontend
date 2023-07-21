455aa781ff0f18c4bdfcdc27adccfdad8f059a5f
bigmosi <kinyeramo@gmail.com>
2023-07-21 12:38:48 +0300

    implemented user name

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
 let isAuthenticated = false;
+let user = null; // Store the user data here
 
 export const checkAuthentication = () => {
   return isAuthenticated;
+};
+
+export const getUser = () => {
+  return user;
 };
 
 export const login = async (username, password) => {
@@ -12,13 +17,18 @@ export const login = async (username, password) => {
       username,
       password,
     });
+    console.log('Login Response:', response.data);
 
-    // Assuming the API returns a successful response with an access_token
+
     // Save the access token to local storage
     localStorage.setItem('access_token', response.data.access_token);
 
     // Set isAuthenticated to true upon successful login
     isAuthenticated = true;
+
+    // Save the user data in the variable
+    user = response.data.user;
+    console.log('User:', user);
 
     return response.data;
   } catch (error) {
